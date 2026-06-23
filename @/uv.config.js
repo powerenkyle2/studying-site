3c74f793ee3eb792d9b1dfae10169593b9d05cee
powerenkyle2 <powerenkyle2@gmail.com>
2026-06-23 16:15:38 +0800

    Change handler and client URLs to CDN links
    Updated URLs in uv.config.js to point to CDN resources.

--- @/uv.config.js
+++ @/uv.config.js
@@ -1,11 +1,11 @@
 self.__uv$config = {
 	prefix: '/@/',
-	bare: '/bare/',
+	bare: '/',
 	encodeUrl: Ultraviolet.codec.xor.encode,
 	decodeUrl: Ultraviolet.codec.xor.decode,
-	handler: '/@/uv.handler.js',
-	client: '/@/uv.client.js',
-	bundle: '/@/uv.bundle.js',
-	config: '/@/uv.config.js',
-	sw: '/@/uv.sw.js'
+	handler: 'https://cdn.jsdelivr.net/gh/powerenkyle2/studying-site@main/%40/uv.handler.js',
+	client: 'https://cdn.jsdelivr.net/gh/powerenkyle2/studying-site@main/%40/uv.client.js',
+	bundle: 'https://cdn.jsdelivr.net/gh/powerenkyle2/studying-site@main/%40/uv.bundle.js',
+	config: 'https://cdn.jsdelivr.net/gh/powerenkyle2/studying-site@main/%40/uv.config.js',
+	sw: 'https://cdn.jsdelivr.net/gh/powerenkyle2/studying-site@main/%40/uv.sw.js'
 };
